0b651f5b5dd95f8bd964253779ef73172f26e0fe
Da Wei <xichuanglian@gmail.com>
2013-07-02 12:55:34 +0800

    simulated annealing with mpi

---
+++ mpi/annealing_mpi.py
@@ -0,0 +1,109 @@
+import random
+import math
+import time
+from dataset import Dataset
+from mpi4py import MPI
+
+def temperature(t):
+    return 0.8 * t
+
+def generate_solution(core_num,rotamer_num):
+    data.load_library()
+    s = []
+    for i in range(core_num):
+        l = [random.randrange(x) for x in rotamer_num]
+        v = data.calc_energy(l)
+        s.append((l,v))
+    return s
+
+def split(n_and_b):
+    best = ([],0)
+    l = []
+    for s,b in n_and_b:
+        l.append(s)
+        if b[1] < best[1]:
+            best = b
+    return (l,best)
+
+def accept(new_v,old_v,t):
+    if new_v <= old_v:
+        return True
+    else:
+        d = new_v - old_v
+        return math.exp(t*d/new_v) < random.random()
+
+def average(s):
+    sum_v = 0.0
+    for l,v in s:
+        sum_v += v
+    return sum_v / len(s)
+
+def sub_annealing(data,s,T):
+    sub_best = ([],0.0)
+    t = T
+    min_t = T * 0.1
+    while t > min_t:
+        old_node,old_v = s
+        new_node = old_node[:]
+        ri = random.randrange(data.residue_num)
+        new_node[ri] = random.randrange(data.rotamer_num[ri])
+        new_v = data.calc_energy(new_node)
+        if accept(new_v,old_v,t):
+            s = (new_node,new_v)
+        if new_v < sub_best[1]:
+            sub_best = (new_node,new_v)
+        t *= 0.999
+    return (s,sub_best)
+
+def sim_annealing(comm,data,core_num):
+    n = data.residue_num
+    rot_n = data.rotamer_num
+    solution = generate_solution(core_num,data.rotamer_num)
+    avg_s = average(solution)
+    best = ([],10.0)
+    T = 100.0
+    stable_count = 0
+    while T > 0.001 and stable_count < 10:
+        T = comm.bcast(T,root=0)
+        s = comm.scatter(solution,root=0)
+        s = sub_annealing(data,s,T)
+        new_s_and_best = comm.gather(s,root=0)
+        new_s,b = split(new_s_and_best)
+        new_avg = average(new_s)
+        if accept(new_avg,avg_s,T):
+            solution = new_s
+            avg_s = new_avg
+        if b[1] < best[1]:
+            best = b
+            stable_count = 0
+        stable_count += 1
+        T = T * 0.6
+    T = -1
+    T = comm.bcast(T,root=0)
+    return best
+
+def sim_annealing_worker(comm,data):
+    T = None
+    T = comm.bcast(T,root=0)
+    while T > 0:
+        s = None
+        s = comm.scatter(s,root=0)
+        s = sub_annealing(data,s,T)
+        s = comm.gather(s,root=0)
+        T = comm.bcast(T,root=0)
+
+if __name__ == '__main__':
+    data = Dataset("rotamerLibrary", "energyTable")
+    data.load_library()
+    comm = MPI.COMM_WORLD
+    size = comm.Get_size()
+    rank = comm.Get_rank()
+
+    if rank == 0:
+        start = time.time()
+        result = sim_annealing(comm,data,size)
+        elapsed = time.time() - start
+        print result
+        print elapsed
+    else:
+        sim_annealing_worker(comm,data)
---
+++ mpi/dataset.py
@@ -0,0 +1,62 @@
+from ctypes import *
+
+class Dataset:
+    def __init__(self, rlib_name, elib_name):
+        #self.lib = cdll.LoadLibrary("./utils.so")
+        #self.lib.calc_g_delta.restype = c_float
+        #self.lib.calc_h.restype = c_float
+
+        # read the number of rotamers for each residue
+        rlib_file = open(rlib_name)
+        self.residue_num = int(rlib_file.readline())
+        self.rotamer_num = [int(x) for x in rlib_file.readline().split()]
+        assert self.residue_num == len(self.rotamer_num)
+        rlib_file.close()
+        # print self.rotamer_num
+
+        # build the offset list
+        self.offset = [-1] * (self.residue_num ** 2)
+        s = 0
+
+        # read the energy table
+        elib_file = open(elib_name)
+        self.energy = []
+        assert int(elib_file.readline()) == self.residue_num
+        for line in elib_file:
+            ls = line.split()
+            a = int(ls[0])
+            b = int(ls[1])
+            nums = [float(x) for x in ls[2:]]
+            assert len(nums) == self.rotamer_num[a] * self.rotamer_num[b]
+            self.energy.extend(nums)
+            self.offset[a * self.residue_num + b] = s
+            s += len(nums)
+        elib_file.close()
+
+        #self.rotamer_num_c = (c_int * self.residue_num)(*self.rotamer_num)
+        #self.offset_c = (c_int * len(self.offset))(*self.offset)
+        #self.energy_c = (c_float * len(self.energy))(*self.energy)
+
+    def load_library(self):
+        self.lib = cdll.LoadLibrary("./utils.so")
+        self.lib.calc_g_delta.restype = c_float
+        self.lib.calc_h.restype = c_float
+        self.lib.calc_energy.restype = c_float
+        self.rotamer_num_c = (c_int * self.residue_num)(*self.rotamer_num)
+        self.offset_c = (c_int * len(self.offset))(*self.offset)
+        self.energy_c = (c_float * len(self.energy))(*self.energy)
+
+    def calc_g_delta(self, nodes):
+        nodes_c = (c_int * len(nodes))(*nodes)
+        return self.lib.calc_g_delta(byref(nodes_c), len(nodes), self.residue_num,
+                                     byref(self.rotamer_num_c), byref(self.offset_c), byref(self.energy_c))
+
+    def calc_h(self, nodes):
+        nodes_c = (c_int * len(nodes))(*nodes)
+        return self.lib.calc_h(byref(nodes_c), len(nodes), self.residue_num,
+                               byref(self.rotamer_num_c), byref(self.offset_c), byref(self.energy_c))
+
+    def calc_energy(self, nodes):
+        nodes_c = (c_int * len(nodes))(*nodes)
+        return self.lib.calc_energy(byref(nodes_c), self.residue_num,
+                                    byref(self.rotamer_num_c), byref(self.offset_c), byref(self.energy_c))
